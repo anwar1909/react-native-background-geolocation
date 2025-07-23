27a273940e62d93f92ba8bef08bfae5b015677c9
anwar1909 <anwar.sadad1909@gmail.com>
2025-07-23 16:17:34 +0700

    fix

--- react-native.config.js
+++ react-native.config.js
@@ -1,19 +1,8 @@
 module.exports = {
-  dependency: {
-    '@anwar1909/react-native-background-geolocation': {
-      root: __dirname,
-      platforms: {
-        android: {
-          sourceDir: 'android/lib',
-        },
-      },
-    },
-    '@anwar1909/react-native-background-geolocation-common': {
-      root: __dirname,
-      platforms: {
-        android: {
-          sourceDir: 'android/common',
-        },
+  dependencies: {
+    platforms: {
+      android: {
+        sourceDir: 'android/lib',
       },
     },
   },
